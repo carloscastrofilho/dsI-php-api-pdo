0510e287e1118a5f345d1eeee82e82ca450de58b
Carlos Pereira de Castro Filho <50716871+carloscastrofilho@users.noreply.github.com>
2024-09-13 10:39:40 -0300

    Update add_task.php

--- add_task.php
+++ add_task.php
@@ -1,19 +1,22 @@
 <?php
-
 require 'database.php';
-
-if (isset($_POST['title'])) {
-    $title = $_POST['title'];
+$data = json_decode(file_get_contents('php://input'), true);
+if (isset($data[ "nome"])) {
+    $nome = $data['nome'];
+    $telefone = $data['telefone'];
+    $observacao = $data['observacao'];
 
     try {
-        $stmt = $conn->prepare('INSERT INTO tasks (title) VALUES (:title)');
-        $stmt->bindParam(':title', $title);
+        $stmt = $conn->prepare('INSERT INTO pessoas (nome,telefone,observacao) VALUES (:nome, :telefone, :observacao)');
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':observacao', $observacao);
         $stmt->execute();
-        $taskId = $conn->lastInsertId();
-        echo json_encode(['id' => $taskId, 'title' => $title, 'completed' => false]);
+        $pessoaId = $conn->lastInsertId();
+        echo json_encode(['id' => $pessoaId, 'nome' => $nome, 'telefone' => $telefone ]);
     } catch(PDOException $e) {
         echo json_encode(['error' => $e->getMessage()]);
     }
 } else {
-    echo json_encode(['error' => 'O título da tarefa é obrigatório']);
+    echo json_encode(['error' => 'O nome é obrigatório']);
 }
